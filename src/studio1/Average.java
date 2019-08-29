626057dbc15e5a6442b4a9fcaaacd5a3d5d4ad90
Benn Oishi <boishi@wustl.edu>
2019-08-29 13:48:47 -0500

    Finsihed Average

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -5,9 +5,9 @@ import support.cse131.ArgsProcessor;
 public class Average {
     public static void main(String[] args){
         ArgsProcessor ap = new ArgsProcessor(args);
-        double n1 = ap.nextDouble("The first of two integers to be averaged?");
-        double n2 = ap.nextDouble("The second of two integers to be averaged?");
+        double n1 = ap.nextDouble("The first of two numbers to be averaged?");
+        double n2 = ap.nextDouble("The second of two numbers to be averaged?");
         double average = ((n1 + n2) / 2);
-        System.out.println(average);
+        System.out.println("The average is: " + average);
     }
 }
